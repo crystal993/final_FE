b253c572bd5148ecf7e55f9816d6f07b29f43272
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-08-31 02:03:10 +0900

    [crystal993] <feat> global header 완성

--- src/components/elements/GlobalFooter.jsx
+++ src/components/elements/GlobalFooter.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
 const GlobalFooter = () => {
-  return <div>GlobalFooter</div>;
+  return (
+    <>
+      <Footer>footer</Footer>
+    </>
+  );
 };
+
+const Footer = styled.footer`
+  background-color: ${({ theme }) => theme.mainColor};
+  width: 100%;
+  height: 200px;
+  text-align: center;
+  margin: 0 auto;
+`;
 
 export default GlobalFooter;
