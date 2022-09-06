bb41f683cfa18c9aba5713cbfc475e256adcb594
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-06 15:46:29 +0900

    <chore> React.StrictMode 제거

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={lightTheme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
