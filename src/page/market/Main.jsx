1933a7ec93b179227336afe99f077847def3b38a
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-08 13:14:35 +0900

    <fix> AddPostButton import 에러

--- src/page/market/Main.jsx
+++ src/page/market/Main.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Header from '../../components/elements/GlobalHeader';
-import Footer from '../../components/elements/GlobalFooter';
-import Layout from '../../components/elements/GlobalLayout';
-import MainContainer from '../../components/market/main/MainContainer';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import Header from "../../components/elements/GlobalHeader";
+import Footer from "../../components/elements/GlobalFooter";
+import Layout from "../../components/elements/GlobalLayout";
+import MainContainer from "../../components/market/main/MainContainer";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import AddPostButton from "../../components/elements/buttons/AddPostButton";
 
 const Main = () => {
   const navigate = useNavigate();
