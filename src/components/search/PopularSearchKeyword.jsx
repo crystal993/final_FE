f7256f0af8b7e64de9b421d4b69d3a43029d2361
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-09 22:14:33 +0900

    <fix> 인기 검색어 검색 기능

--- src/components/search/PopularSearchKeyword.jsx
+++ src/components/search/PopularSearchKeyword.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { __itemSearch } from "../../redux/modules/searchSlice";
 
 const PopularSearchKeyword = ({ keyword }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // 검색 기능
+  const onSearchResultHandler = (keyword) => {
+    dispatch(__itemSearch({ keyword }));
+    navigate(`/search/result/${keyword}`);
+  };
+
   return (
     <KeywordWrapper>
-      <Keyword>{keyword}</Keyword>
+      <Keyword onClick={() => onSearchResultHandler(keyword)}>
+        {keyword}
+      </Keyword>
     </KeywordWrapper>
   );
 };
@@ -15,6 +28,7 @@ const KeywordWrapper = styled.div`
   gap: 1rem;
   border: 1px solid ${({ theme }) => theme.mainColor};
   border-radius: 100px;
+  cursor: pointer;
 `;
 const Keyword = styled.span`
   font-weight: 500;
