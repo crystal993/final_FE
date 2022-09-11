4ecb43afe897d9b1a48de4b09c3b3cb47b26c0cc
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-11 23:02:19 +0900

    <fix> MyPage 폴더 구조

--- src/page/myPage/MyPage.jsx
+++ src/page/myPage/MyPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "../components/elements/GlobalHeader";
-import Layout from "../components/elements/GlobalLayout";
-import MyPages from "../components/user/MyPage";
+import Header from "../../components/elements/GlobalHeader";
+import Layout from "../../components/elements/GlobalLayout";
+import MyPages from "../../components/myPage/MyPage";
 const MyPage = () => {
   return (
     <>
