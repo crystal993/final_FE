d3cec53a3dacab907671714c9247fe866f1d708d
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-11 23:04:10 +0900

    <feat> 내가 찜한 리스트 페이지 컴포넌트 생성

---
+++ src/components/myPage/zzim/MyPageZzimContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const MyPageZzimContainer = () => {
+  return <div>card</div>;
+};
+
+export default MyPageZzimContainer;
---
+++ src/page/myPage/MyZzim.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/elements/GlobalLayout";
+import Header from "../../components/elements/GlobalHeader2";
+import MyPageZzimContainer from "../../components/myPage/zzim/MyPageZzimContainer";
+
+const MyZzim = () => {
+  return (
+    <Layout>
+      <Header />
+      <MyPageZzimContainer />
+    </Layout>
+  );
+};
+
+export default MyZzim;
