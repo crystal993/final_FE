2dc8d81f8546a9706bd595b4d84ae6e64b54b4cd
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-11 23:04:36 +0900

    <feat> 최근 본 상품 페이지 컴포넌트 생성

---
+++ src/components/myPage/products/MyViewedProductsContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyViewedProductsContainer = () => {
+  return <div>MyViewedProductsContainer</div>;
+};
+
+export default MyViewedProductsContainer;
---
+++ src/page/myPage/MyViewedProucts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../../components/elements/GlobalLayout";
+import Header from "../../components/elements/GlobalHeader2";
+import MyViewedProductsContainer from "../../components/myPage/products/MyViewedProductsContainer";
+
+const MyViewedProucts = () => {
+  return (
+    <>
+      <Layout>
+        <Header />
+        <MyViewedProductsContainer />
+      </Layout>
+    </>
+  );
+};
+
+export default MyViewedProucts;
