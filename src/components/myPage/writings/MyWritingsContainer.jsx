8dab27fcdb00ab2468b93ee0e0c1154e9bf1033e
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-24 21:24:46 +0900

    <feat> 내가 쓴글 반응형 적용

--- src/components/myPage/writings/MyWritingsContainer.jsx
+++ src/components/myPage/writings/MyWritingsContainer.jsx
@@ -19,22 +19,11 @@ const SectionWrapper = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: 9rem;
+  padding-top: 10rem;
   margin-bottom: 1rem;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  .button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 1rem;
-    gap: 0.5rem;
-  }
 `;
 
 const MyWritingsTitle = styled.h1`
   font-size: 2.4rem;
-  margin: 0 2.4rem;
   font-weight: 700;
 `;
