8564a6bec544032daf1535ef9e2d06f8bca36ad5
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-26 23:44:57 +0900

    <fix> 검색 페이지에 footer 제거

--- src/page/search/Search.jsx
+++ src/page/search/Search.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "../../components/search/SearchHeader";
 import SearchContainer from "../../components/search/Search";
-import Footer from "../../components/elements/GlobalFooter";
 
 const Search = () => {
   return (
     <>
       <Header />
       <SearchContainer />
-      <Footer />
     </>
   );
 };
