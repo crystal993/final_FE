cb29682331363631b7def30005ffdb5fd8f840b4
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-26 23:44:36 +0900

    <fix> 찜페이지 footer 적용 위해 Wrapper 적용

--- src/page/myPage/MyZzim.jsx
+++ src/page/myPage/MyZzim.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import Footer from "../../components/elements/GlobalFooter";
 import Header from "../../components/elements/GlobalHeader2";
 import MyPageZzimContainer from "../../components/myPage/zzim/MyPageZzimContainer";
+import styled from "styled-components";
 
 const MyZzim = () => {
   return (
     <>
-      <Header />
-      <MyPageZzimContainer />
-      <Footer />
+      <Wrapper>
+        <Header />
+        <MyPageZzimContainer />
+        <Footer />
+      </Wrapper>
     </>
   );
 };
 
 export default MyZzim;
+
+const Wrapper = styled.div`
+  height: auto;
+  min-height: 100%;
+  padding-bottom: 0rem;
+`;
