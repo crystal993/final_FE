ecc5a3c7dd1bfad330e23baa18d7278e9cfeb0e6
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-27 18:49:52 +0900

    <fix> 회원가입 페이지 css 변경 및 반응형 적용

--- src/page/register/Signup.jsx
+++ src/page/register/Signup.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import SignupForm from '../../components/register/SignupForm';
-import GlobalLayout from './../../components/elements/GlobalLayout';
+import React from "react";
+import SignupForm from "../../components/register/SignupForm";
+import Layout from "./../../components/elements/GlobalLayout";
+import Header from "../../components/elements/GlobalHeader2";
 
 const Signup = () => {
   return (
-    <GlobalLayout>
-      <SignupForm />
-    </GlobalLayout>
+    <>
+      <Header />
+      <Layout>
+        <SignupForm />
+      </Layout>
+    </>
   );
 };
 
