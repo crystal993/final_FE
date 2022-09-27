35855e485b600686a548434f60295a0b89f4c0d7
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-09-27 23:01:08 +0900

    <fix> 댓글 css

--- src/components/market/comment/Comment.jsx
+++ src/components/market/comment/Comment.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getCommentData,
   postCommentData,
   deleteCommentData,
   putCommentData,
-} from '../../../redux/modules/market/commentSlice';
-import GlobalButton from './../../elements/GlobalButton';
-
+} from "../../../redux/modules/market/commentSlice";
+import GlobalButton from "./../../elements/GlobalButton";
+import { ReactComponent as ProfileIcon } from "../../../assets/icons/profile_img_sm.svg";
 const Comment = ({ id }) => {
   const dispatch = useDispatch();
 
@@ -19,7 +19,7 @@ const Comment = ({ id }) => {
   console.log(comment_data);
 
   //   입력받은 값
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const onChangeHandler = (event) => {
     setInput(event.target.value);
@@ -33,7 +33,7 @@ const Comment = ({ id }) => {
       content: input,
     };
     dispatch(postCommentData(body));
-    setInput('');
+    setInput("");
   };
 
   //   컴포넌트 마운트 시에 id에 해당하는 댓글을 가져옴
@@ -46,8 +46,8 @@ const Comment = ({ id }) => {
       <Wrapper>
         <Label>
           <Input
-            placeholder='댓글을 입력해 주세요'
-            name='input'
+            placeholder="댓글을 입력해 주세요"
+            name="input"
             value={input}
             onChange={onChangeHandler}
           />
@@ -56,41 +56,53 @@ const Comment = ({ id }) => {
           </button>
         </Label>
         {comment_data.length === 0 ? (
-          <section className='no-comment'>
+          <section className="no-comment">
             <p>아직 댓글이 없어요</p>
             <p>가장 먼저 댓글을 남겨 보세요.</p>
           </section>
         ) : (
           comment_data &&
           comment_data.map((item, index) => (
-            <section key={index} className='comment-section'>
-              <h2>{item.nickname}</h2>
-              <p>{item.content}</p>
-              {/* ismine인지 확인하여 버튼 조건부 랜더링 */}
-              <button
-                content={'수정'}
-                onClick={() => {
-                  dispatch(
-                    putCommentData({
-                      itemId: id,
-                      content: input,
-                      commentId: item.commentId,
-                    })
-                  );
-                  setInput('');
-                }}
-              >
-                수정
-              </button>
-              <button
-                onClick={() => {
-                  dispatch(
-                    deleteCommentData({ itemId: id, commentId: item.commentId })
-                  );
-                }}
-              >
-                삭제
-              </button>
+            <section key={index} className="comment-section">
+              <StUserBox>
+                <UserImgBox>
+                  <StProfileIcon />
+                </UserImgBox>
+                <UserInfoTxt>
+                  <H3>{item.nickname}</H3>
+                  <P>{item.content}</P>
+                </UserInfoTxt>
+              </StUserBox>
+              <ButtonsWrapper>
+                {/* ismine인지 확인하여 버튼 조건부 랜더링 */}
+                <button
+                  content={"수정"}
+                  onClick={() => {
+                    dispatch(
+                      putCommentData({
+                        itemId: id,
+                        content: input,
+                        commentId: item.commentId,
+                      })
+                    );
+                    setInput("");
+                  }}
+                >
+                  수정
+                </button>
+                <button
+                  onClick={() => {
+                    dispatch(
+                      deleteCommentData({
+                        itemId: id,
+                        commentId: item.commentId,
+                      })
+                    );
+                  }}
+                >
+                  삭제
+                </button>
+              </ButtonsWrapper>
             </section>
           ))
         )}
@@ -109,16 +121,15 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding-bottom: 10rem;
-
+  padding-bottom: 8rem;
+  margin: 0 auto;
   .comment-section {
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
-    width: 100%;
-    padding: 20px;
-    margin-left: 10rem;
-    gap: 10px;
+    width: 36rem;
+    padding: 1.4rem 1.8rem;
+    gap: 1rem;
 
     button {
       cursor: pointer;
@@ -126,6 +137,11 @@ const Wrapper = styled.div`
 
     button:hover {
       background: #f0f0f0;
+    }
+    @media (max-width: 767px) {
+      /* Mobile */
+      padding: 0.5rem 2.2rem;
+      width: 25rem;
     }
   }
 
@@ -143,22 +159,10 @@ const Wrapper = styled.div`
     bottom: 0;
     left: 0;
     right: 0;
-    @media screen and (min-width: 1024px) {
-      /* Desktop */
-      width: 90%;
-    }
-    @media screen and (min-width: 768px) and (max-width: 1023px) {
-      /* Tablet */
-      width: 95%;
-    }
-
-    @media screen and (max-width: 767px) {
-      /* Mobile */
-      width: 98%;
-    }
     margin: 0 auto;
     background: #b63eff;
     padding: 1rem;
+
     span {
       font-weight: 700;
       font-size: 1.6rem;
@@ -183,11 +187,28 @@ const Input = styled.input`
   margin-left: 1.6rem;
   margin-right: 1.6rem;
   font-size: 1.4rem;
+  padding: 0.5rem;
+  outline: none;
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 1000px white inset;
+  }
+  @media (max-width: 767px) {
+    /* Mobile */
+    margin-top: 3.4rem;
+    padding: 0.8rem;
+    font-size: 1.1rem;
+    width: 21rem;
+    height: 3rem;
+  }
 `;
 
 const Label = styled.label`
   position: relative;
   margin-bottom: 5rem;
+  @media (max-width: 767px) {
+    /* Mobile */
+    margin-bottom: 4rem;
+  }
   button {
     position: absolute;
     top: 50%;
@@ -203,10 +224,112 @@ const Label = styled.label`
     span {
       color: #ffffff;
     }
+    @media (max-width: 767px) {
+      /* Mobile */
+      top: 61%;
+      right: 1%;
+      width: 4rem;
+      height: 2rem;
+    }
   }
 
   button:hover {
     cursor: pointer;
     background-color: lightgray;
+  }
+`;
+
+const StUserBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  justify-content: flex-start;
+  column-gap: 1.2rem;
+  row-gap: 1.2rem;
+  margin-bottom: 3rem;
+  @media screen and (min-width: 1024px) {
+    /* Desktop */
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    /* Tablet */
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+  @media (max-width: 767px) {
+    /* Mobile */
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+`;
+
+const UserImgBox = styled.div`
+  border-radius: 70%;
+  overflow: hidden;
+  width: 4rem;
+  height: 4rem;
+  @media (max-width: 767px) {
+    /* Mobile */
+    width: 2.5rem;
+    height: 2.5rem;
+  }
+`;
+
+const StProfileIcon = styled(ProfileIcon)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  path {
+    fill: white;
+    background-color: #6b6b6b;
+  }
+  circle {
+    fill: #6b6b6b;
+  }
+`;
+
+const UserInfoTxt = styled.div`
+  margin-top: -0.5rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.3rem;
+`;
+
+const P = styled.p`
+  font-size: 1.2rem;
+  @media (max-width: 767px) {
+    /* Mobile */
+    font-size: 1rem;
+  }
+`;
+
+const H3 = styled.p`
+  font-weight: bold;
+  font-size: 1.6rem;
+  @media (max-width: 767px) {
+    /* Mobile */
+    font-size: 1.2rem;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+  margin-top: -5rem;
+  display: flex;
+  flex-direction: row;
+  column-gap: 1rem;
+
+  button {
+    border: none;
+    font-size: 1rem;
+    color: ${({ theme }) => theme.darkgray};
+    @media (max-width: 767px) {
+      /* Mobile */
+      font-size: 0.8rem;
+    }
   }
 `;
