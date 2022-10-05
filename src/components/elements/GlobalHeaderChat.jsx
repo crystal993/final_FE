c4b1f5bae29c4f4d70c3b97633cb525f0aaad74f
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-10-05 17:17:19 +0900

    <chore> chatRoom 페이지 라우팅

--- src/components/elements/GlobalHeaderChat.jsx
+++ src/components/elements/GlobalHeaderChat.jsx
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { ReactComponent as ArrowBackIcon } from '../../assets/icons/arrow_back_ios.svg';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { ReactComponent as ArrowBackIcon } from "../../assets/icons/arrow_back_ios.svg";
+import { ReactComponent as HomeIcon } from "../../assets/icons/home.svg";
 
 const GlobalHeaderChat = () => {
   const navigate = useNavigate();
 
-  const onPathHandler = (path) => {
-    navigate(path);
-  };
+  const member = localStorage.getItem("user-info");
+  const obj = JSON.parse(member);
+  const nickname = obj.nickname;
 
   return (
     <NavbarWrapper>
       <Navbar>
         <NavItem>
-          <div className='nickname-wrapper'>
-            <ArrowBackIcon onClick={() => onPathHandler('/')} />
-            <span className='chat-nickname'>닉네임</span>
-          </div>
+          <ArrowBackIcon onClick={() => navigate(-1)} />
+          {nickname}
         </NavItem>
-        <NavItem onClick={() => onPathHandler('/')}>
-          <span className='logo'>LOGO</span>
-        </NavItem>
+        <NavItem></NavItem>
         <NavItem>
-          <button>나가기</button>
+          <ExitButton>나가기</ExitButton>
         </NavItem>
       </Navbar>
     </NavbarWrapper>
@@ -33,9 +30,13 @@ const GlobalHeaderChat = () => {
 export default GlobalHeaderChat;
 
 const NavbarWrapper = styled.div`
+  position: fixed;
+  height: 4.8rem;
+  width: 100%;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  z-index: 5;
 `;
 
 const Navbar = styled.nav`
@@ -46,8 +47,9 @@ const Navbar = styled.nav`
   padding: 20px;
   align-items: center;
   height: 4.8rem;
-  background-color: ${({ theme }) => theme.mainColor};
-  color: white;
+  background-color: ${({ theme }) => theme.headerMainColor};
+  color: ${({ theme }) => theme.headerTxtColor};
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const NavItem = styled.div`
@@ -59,18 +61,25 @@ const NavItem = styled.div`
   gap: 2rem;
   cursor: pointer;
   width: fit-content;
+  color: black;
+`;
 
-  .nickname-wrapper {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .chat-nickname {
-      font-size: 1rem;
-    }
+const ExitButton = styled.button`
+  background: #9e9e9e;
+  color: white;
+  padding: 0.7rem;
+  border: none;
+  border-radius: 0.4rem;
+  @media (min-width: 1280px) {
+    /* Desktop */
+    width: 9.3rem;
   }
-
-  .logo {
-    margin-right: 3rem;
+  @media (min-width: 768px) and (max-width: 1280px) {
+    /* Tablet */
+    width: 9.3rem;
+  }
+  @media (max-width: 767px) {
+    /* Mobile */
+    width: 8rem;
   }
 `;
